c71b473fb72eed7b88c831b22213d90db840b44e
Alaldeen Sourjah <alaldeensourjah@gmail.com>
2025-07-03 23:26:24 +0000

    meterials page not loading

--- src/components/add-material-form.tsx
+++ src/components/add-material-form.tsx
@@ -61,10 +61,10 @@ export function AddMaterialForm({ onAddMaterial, onUpdateMaterial, materialToEdi
     } else {
       form.reset({ name: '', quantity: 0, costPerUnit: 0 });
     }
-  }, [materialToEdit, form]);
+  }, [materialToEdit, form.reset]);
 
   function onSubmit(values: z.infer<typeof materialSchema>) {
-    if (isEditMode) {
+    if (isEditMode && materialToEdit) {
       onUpdateMaterial({ ...values, id: materialToEdit.id });
     } else {
       onAddMaterial(values);
