72920a85cfbfeffb95f8255853402a820043aa51
Alaldeen Sourjah <alaldeensourjah@gmail.com>
2025-07-06 15:15:33 +0000

    ⚠ Cross origin request detected from 192.168.1.90 to /_next/* resource.

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: ['http://localhost:9002', 'http://192.168.1.90:9002'],
+  },
+};
+
+export default nextConfig;
